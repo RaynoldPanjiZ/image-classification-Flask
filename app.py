e7aa46338bcb6bbab2b63b5362905616df4bf625
Raynold P <raynoldpanji922@gmail.com>
2022-08-17 04:42:23 +0700

    edit flask

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-from flask import Flask, jsonify, request, redirect, url_for, render_template
+from email import message
+from flask import Flask, jsonify, request, redirect, url_for, render_template, flash
 import keras
 from keras_preprocessing import image
 # from keras.models import load_model
 import os
 import numpy as np
 import json
-
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'static/uploads/'
@@ -20,12 +21,31 @@ def index():
 @app.route('/predict', methods=["POST"])
 def upload():
     file = request.files['file']
+    if check_format(file) == 0:
+        msg = "Allowed image types are -> png, jpg, jpeg, gif"
+        return render_template('index.html', message=msg)
+        
+    # filename = secure_filename(file.filename)
     file_path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
     file.save(file_path)
-    path = "http://127.0.0.1:5000/"+file_path
+    path = os.path.join(request.url_root, file_path)
     pred = classify(file_path)
     return render_template('index.html', filename=file.filename, pred=pred, path=path)
-    
+
+
+
+#### cek format gambar ####
+
+def check_format(file):
+    formats = set(['png', 'jpg', 'jpeg', 'gif'])
+    extention = '.' in file.filename and file.filename.rsplit('.', 1)[1].lower() in formats
+    if file and extention:
+        return 1
+    else:
+	    return 0
+
+
+#### tampilkan gambar ####
 
 @app.route('/display/<filename>')
 def display_image(filename):
@@ -40,19 +60,27 @@ def predict():
 
     if request.method == "POST":
         file = request.files['file']
-        file_path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
-        file.save(file_path)
+        check_format(file)
+        if check_format(file) == 0:
+            res = {
+                'filename': file.filename,
+                'message': "Allowed image types are -> png, jpg, jpeg, gif"
+            }
+        else:
+            file_path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+            file.save(file_path)
 
-        pred = classify(file_path)
+            pred = classify(file_path)
 
-        prediction = {
-            'filename': file.filename,
-            'path': "http://127.0.0.1:5000/"+file_path,
-            'prediksi': pred[1],
-            'kemiripan': str(pred[0]), 
-            'semua_kemiripan': pred[2]
-        }
-        return jsonify(prediction)
+            res = {
+                'filename': file.filename,
+                'path': os.path.join(request.host_url, file_path),
+                'prediksi': pred[1],
+                'kemiripan': str(pred[0]), 
+                'semua_kemiripan': pred[2]
+            }
+
+        return jsonify(res)
     return jsonify(request='404')
     
 
