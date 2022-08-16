945fdbe84f97957602267ff6a050a97286b3b9e0
Raynold P <raynoldpanji922@gmail.com>
2022-08-17 03:27:10 +0700

    edit backend

--- app.py
+++ app.py
@@ -1,34 +1,64 @@
-from flask import Flask, render_template, request, redirect, url_for
-import os
+from flask import Flask, jsonify, request, redirect, url_for, render_template
+import keras
 from keras_preprocessing import image
-from tensorflow.keras.models import load_model
+# from keras.models import load_model
+import os
 import numpy as np
 import json
 
+
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'static/uploads/'
+
+
+#### Front End ####
 
 @app.route('/')
 def index():
     return render_template("index.html")
 
-UPLOAD_FOLDER = 'static/uploads/'
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-@app.route('/', methods=["POST"])
+@app.route('/predict', methods=["POST"])
 def upload():
-    file = request.files['gambar']
+    file = request.files['file']
     file_path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
     file.save(file_path)
-    msg = "File Terupload di: "+file_path
-    pred = predict(file_path)
-    return render_template('index.html', filename=file.filename, pred=pred, message=msg)
+    path = "http://127.0.0.1:5000/"+file_path
+    pred = classify(file_path)
+    return render_template('index.html', filename=file.filename, pred=pred, path=path)
     
 
 @app.route('/display/<filename>')
 def display_image(filename):
-    #print('display_image filename: ' + filename)
     return redirect(url_for('static', filename='uploads/'+filename), code=301)
+
+
+
+#### API response (Backend) ####
+
+@app.route('/api/predict', methods=["POST"])
+def predict():
+
+    if request.method == "POST":
+        file = request.files['file']
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+        file.save(file_path)
+
+        pred = classify(file_path)
+
+        prediction = {
+            'filename': file.filename,
+            'path': "http://127.0.0.1:5000/"+file_path,
+            'prediksi': pred[1],
+            'kemiripan': str(pred[0]), 
+            'semua_kemiripan': pred[2]
+        }
+        return jsonify(prediction)
+    return jsonify(request='404')
     
-    
+
+
+#### Model Prediction ####
+
 def convert_image(path):
     img = image.load_img(path, target_size=(150, 150))
 
@@ -38,21 +68,28 @@ def convert_image(path):
     return np.vstack([x])
 
 
-def predict(path):
+def classify(path):
     images = convert_image(path)
 
     labels = json.load(open('model/labels_map.json'))
     class_names = list(labels.keys())
 
-    savedModel = load_model('model/model.h5')
+    savedModel = keras.models.load_model('model/model.h5')
     pred = savedModel.predict(images)
 
     proba = np.max(pred[0], axis=-1)
+    proba_all = {b:str(a) for a,b in zip(pred[0], class_names)}
     predicted_class = class_names[np.argmax(pred[0], axis=-1)]
+    # if proba >= 0.5:
+    #     predicted_class = class_names[np.argmax(pred[0], axis=-1)]
+    # else:
+    #     predicted_class = "tidak ada yang mirip"
+    #     proba = 0
 
-    prediction = [proba, predicted_class]
+    prediction = [proba, predicted_class, proba_all]
     return prediction
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
